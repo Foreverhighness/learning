f796919d194cf21307862d064031c57933390bc8
Foreverhighness <Foreverhighness@github.com>
2024-01-10 01:30:14 +0800

    🐛 (external-sort) improve portability.

--- experiment/external-sort/cg.sh
+++ experiment/external-sort/cg.sh
@@ -5,9 +5,10 @@ USER=$(whoami)
 GROUP_NAME="external-sort"
 PROGRAM="build/main"
 
-if ! cgget -r memory.limit_in_bytes -n $GROUP_NAME; then
+if ! cgget -r memory.limit_in_bytes -n $GROUP_NAME |& grep limit_in_bytes; then
   sudo cgcreate -t "$USER:$USER" -a "$USER:$USER" -g memory:$GROUP_NAME
   cgset -r memory.limit_in_bytes=1M $GROUP_NAME
+  cgget -r memory.limit_in_bytes -n $GROUP_NAME
 fi
 
 if [[ $# -eq 1 ]]; then
