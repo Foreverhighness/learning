96e320cc0747f3fb77ad06988d0ce3148211c56f
Foreverhighness <Foreverhighness@github.com>
2024-10-25 16:55:08 +0800

    ✨ (art-rs): Add art-rs cli.

---
+++ experiment/art-rs/src/lib.rs
@@ -0,0 +1,99 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::{boxed::Box, vec};
+
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg64Mcg;
+
+type ColorType = u8;
+pub const SCALE_FACTOR: u32 = 100_000;
+pub const MAX_COLOR: usize = ColorType::MAX as _;
+
+#[derive(Debug)]
+struct ColorMap {
+    data: Box<[ColorType]>,
+    height: usize,
+    width: usize,
+}
+
+impl ColorMap {
+    fn new(width: usize, height: usize) -> Self {
+        Self {
+            data: vec![0; width * height].into_boxed_slice(),
+            height,
+            width,
+        }
+    }
+}
+
+impl core::ops::IndexMut<usize> for ColorMap {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        assert!(i < self.height);
+        let begin = i * self.width;
+        let end = begin + self.width;
+        &mut self.data[begin..end]
+    }
+}
+
+impl core::ops::Index<usize> for ColorMap {
+    type Output = [ColorType];
+
+    fn index(&self, i: usize) -> &Self::Output {
+        assert!(i < self.height);
+        let begin = i * self.width;
+        let end = begin + self.width;
+        &self.data[begin..end]
+    }
+}
+
+#[derive(Debug)]
+pub struct PixelGenerator {
+    colors: [ColorMap; 3],
+    radius: isize,
+    height: usize,
+    width: usize,
+    probability: u32,
+    rng: Pcg64Mcg,
+}
+
+impl PixelGenerator {
+    pub fn new(width: usize, height: usize, radius: isize, probability: u32, seed: u64) -> Self {
+        Self {
+            colors: core::array::from_fn(|_| ColorMap::new(width, height)),
+            radius,
+            height,
+            width,
+            probability,
+            rng: Pcg64Mcg::seed_from_u64(seed),
+        }
+    }
+    fn color(&mut self, i: usize, j: usize, color: usize) -> ColorType {
+        if self.colors[color][i][j] == 0 {
+            self.colors[color][i][j] = if self.rng.gen_range(0..SCALE_FACTOR) < self.probability {
+                self.rng.gen()
+            } else {
+                let di = self.rng.gen_range(0..=self.radius);
+                let dj = self.rng.gen_range(0..=self.radius);
+                // let di = self.rng.gen_range(-self.radius..=self.radius);
+                // let dj = self.rng.gen_range(-self.radius..=self.radius);
+                self.color(
+                    i.saturating_add_signed(di) % self.height,
+                    j.saturating_add_signed(dj) % self.width,
+                    color,
+                )
+            };
+        }
+        self.colors[color][i][j]
+    }
+    pub fn red(&mut self, i: usize, j: usize) -> ColorType {
+        self.color(i, j, 0)
+    }
+    pub fn green(&mut self, i: usize, j: usize) -> ColorType {
+        self.color(i, j, 1)
+    }
+    pub fn blue(&mut self, i: usize, j: usize) -> ColorType {
+        self.color(i, j, 2)
+    }
+}
---
+++ experiment/art-rs/src/main.rs
@@ -0,0 +1,66 @@
+use clap::{arg, command, Parser};
+
+use std::io::{BufWriter, Write};
+
+use art_rs::{PixelGenerator, MAX_COLOR, SCALE_FACTOR as SCALE_FACTOR_U32};
+
+const DEFAULT_WIDTH: u16 = 1024;
+const DEFAULT_HEIGHT: u16 = 1024;
+const DEFAULT_RADIUS: u16 = 1;
+const DEFAULT_PROBABILITY: u32 = 100;
+const SCALE_FACTOR: i64 = SCALE_FACTOR_U32 as _;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The width of output picture
+    #[arg(short = 'W', long, default_value_t = DEFAULT_WIDTH)]
+    width: u16,
+
+    /// The height of output picture
+    #[arg(short = 'H', long, default_value_t = DEFAULT_HEIGHT)]
+    height: u16,
+
+    /// The radius of random walk
+    #[arg(short, long, default_value_t = DEFAULT_RADIUS, value_parser = clap::value_parser!(u16).range(1..))]
+    radius: u16,
+
+    /// The probability to generate one pixel, scale by a factor of 100000, means 1000 -> 1%, default 100 -> 0.1%
+    #[arg(short, long, default_value_t = DEFAULT_PROBABILITY, value_parser = clap::value_parser!(u32).range(1..=SCALE_FACTOR))]
+    probability: u32,
+
+    /// The seed of rand
+    #[arg(short, long, default_value_t = 0)]
+    seed: u64,
+
+    /// Output picture path
+    path: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let Cli {
+        width,
+        height,
+        radius,
+        probability,
+        seed,
+        path,
+    } = Cli::parse();
+
+    let (width, height) = (width as usize, height as usize);
+
+    let mut gen = PixelGenerator::new(width, height, radius as isize, probability, seed);
+
+    let file = std::fs::File::create(path)?;
+    let mut file = BufWriter::new(file);
+
+    // https://netpbm.sourceforge.net/doc/ppm.html
+    file.write_all(format!("P6\n{width} {height}\n{MAX_COLOR}\n").as_bytes())?;
+    for i in 0..height {
+        for j in 0..width {
+            file.write_all(&[gen.red(i, j), gen.green(i, j), gen.blue(i, j)])?;
+        }
+    }
+
+    Ok(())
+}
