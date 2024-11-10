eb4cf53e58e81badcb18b22d118513f022b02b66
Foreverhighness <Foreverhighness@github.com>
2024-11-10 17:24:37 +0800

    ♻️ (pcre-ffi-rs): Refactor.

--- experiment/pcre-ffi-rs/build.rs
+++ experiment/pcre-ffi-rs/build.rs
@@ -2,7 +2,6 @@ use std::io::Cursor;
 use std::path::{Path, PathBuf};
 
 use bzip2::read::BzDecoder;
-use cmake::Config;
 use http_req::request;
 use sha2::{Digest, Sha256};
 use tar::Archive;
@@ -10,53 +9,71 @@ use tar::Archive;
 const DOWNLOAD_BASE_URL: &str = "https://github.com/PCRE2Project/pcre2/releases/download";
 const PCRE2: &str = "pcre2";
 const VERSION: &str = "10.42";
+const PCRE2_PATH: &str = "pcre2-10.42";
 const SHA256: &str = "8d36cd8cb6ea2a4c2bb358ff6411b0c788633a2a45dabbf1aeb4b701d1b5e840";
 
-const LIB_NAME: &str = "pcre2-8";
+const LIB_BASENAME: &str = "pcre2-8";
+const LIB_NAME: &str = "libpcre2-8";
 const BUILD_TARGET: &str = "pcre2-8-static";
 
 fn main() {
-    let path = get_pcre2();
-    let dst = Config::new(path).build_target(BUILD_TARGET).build();
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("build").display()
-    );
-    println!("cargo:rustc-link-lib=static={LIB_NAME}");
+    if let Err(_) = pkg_config::Config::new()
+        .statik(true)
+        .atleast_version(VERSION)
+        .probe(LIB_NAME)
+    {
+        let build_dir = build_pcre2();
+        println!("cargo:rustc-link-search=native={}", build_dir.display());
+        println!("cargo:rustc-link-lib=static={LIB_BASENAME}");
+    }
+
     println!("cargo:rerun-if-changed=build.rs");
 }
 
-fn get_pcre2() -> PathBuf {
+fn build_pcre2() -> PathBuf {
+    let compressed_file = download_compressed_file();
+    let mut archive = Archive::new(BzDecoder::new(Cursor::new(compressed_file)));
+
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
-    let source_dir = out_dir.join("source");
+    let third_party = out_dir.join("third_party");
+    archive.unpack(&third_party).unwrap();
 
-    let compressed_file = download_compressed_file();
+    let source_dir = {
+        let mut third_party = third_party;
+        third_party.push(PCRE2_PATH);
+        third_party
+    };
 
-    let mut archive = Archive::new(BzDecoder::new(compressed_file));
-    archive.unpack(&source_dir).unwrap();
+    let dst_dir = (cmake::Config::new(source_dir))
+        .build_target(BUILD_TARGET)
+        .build();
 
-    let mut source_dir = source_dir;
-    let basename = format!("{PCRE2}-{VERSION}");
-    source_dir.push(basename);
-    source_dir
+    let build_dir = {
+        let mut dst_dir = dst_dir;
+        dst_dir.push("build");
+        dst_dir
+    };
+
+    build_dir
 }
 
-fn try_download(url: &str) -> Result<Cursor<Vec<u8>>, String> {
+fn try_download(url: &str) -> Result<Vec<u8>, String> {
     let body = {
         let mut body = Vec::new();
         let mut url = url.to_owned();
         for _retry in 0..3 {
             // Send GET request
             let response = request::get(&url, &mut body).map_err(|e| e.to_string())?;
-            if response.status_code().is_redirect() {
+            let status_code = response.status_code();
+            if status_code.is_redirect() {
                 url = response.headers().get("Location").cloned().unwrap();
                 continue;
             }
-            if response.status_code().is_success() {
+            if status_code.is_success() {
                 break;
             }
-            return Err(format!("Download error: HTTP {}", response.status_code()));
+            return Err(format!("Download error: HTTP {status_code}"));
         }
         body
     };
@@ -66,13 +83,13 @@ fn try_download(url: &str) -> Result<Cursor<Vec<u8>>, String> {
     if format!("{hash:x}") != SHA256 {
         return Err(format!("Downloaded {PCRE2} file failed hash check."));
     }
-    Ok(Cursor::new(body))
+
+    Ok(body)
 }
 
-fn download_compressed_file() -> Cursor<Vec<u8>> {
-    let basename = format!("{PCRE2}-{VERSION}");
-    let filename = format!("{basename}.tar.bz2");
-    let url = format!("{DOWNLOAD_BASE_URL}/{basename}/{filename}");
+fn download_compressed_file() -> Vec<u8> {
+    let filename = format!("{PCRE2_PATH}.tar.bz2");
+    let url = format!("{DOWNLOAD_BASE_URL}/{PCRE2_PATH}/{filename}");
 
     try_download(&url).unwrap_or_else(|e| panic!("\n\nDownload error: {e}\n\n"))
 }
--- experiment/pcre-ffi-rs/src/main.rs
+++ experiment/pcre-ffi-rs/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 use std::ffi::{c_void, CString};
+use std::io::BufRead;
 use std::os::raw::{c_int, c_uchar};
 
 type pcre2_code = c_void;
@@ -44,66 +45,74 @@ extern "C" {
 
 fn send_result(result: String) {
     use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-    static CELL: std::sync::OnceLock<UdpSocket> = std::sync::OnceLock::new();
+    static SOCKET: std::sync::LazyLock<UdpSocket> =
+        std::sync::LazyLock::new(|| UdpSocket::bind("127.0.0.1:34254").unwrap());
+    static DST_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 34255);
 
-    let socket = CELL.get_or_init(|| UdpSocket::bind("127.0.0.1:34254").unwrap());
-    let port = 34255;
-    let dst = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
-    println!("send to {dst:?}: {result}");
-    socket.send_to(&result.into_bytes(), dst).unwrap();
+    eprintln!("send to {DST_ADDR:?}: {result}");
+    SOCKET.send_to(&result.into_bytes(), DST_ADDR).unwrap();
 }
 
 const NULL: *mut c_void = std::ptr::null_mut();
 const PCRE2_ZERO_TERMINATED: PCRE2_SIZE = !0;
-use std::io::BufRead;
 
 fn main() {
     let filename = "input.txt";
 
-    unsafe {
-        let re = {
-            let pattern = CString::new(r"\d{4}([^\d\s]{3,11}).").unwrap();
-            println!("pattern: {pattern:?}");
-            let mut errorcode = 0;
-            let mut erroroffset: usize = 0;
+    let re = {
+        // https://doc.rust-lang.org/std/ffi/struct.CString.html#creating-a-cstring
+        let pattern = CString::from(cr"\d{4}([^\d\s]{3,11}).");
+        eprintln!("pattern: {pattern:?}");
+        let mut errorcode = 0;
+        let mut erroroffset: usize = 0;
+        unsafe {
             pcre2_compile_8(
                 pattern.as_ptr().cast(),
                 PCRE2_ZERO_TERMINATED,
                 0,
-                &mut errorcode as _,
-                &mut erroroffset as _,
+                &raw mut errorcode,
+                &raw mut erroroffset,
                 NULL,
             )
-        };
-        debug_assert_ne!(re, NULL);
+        }
+    };
+    debug_assert_ne!(re, NULL);
 
-        let f = std::io::BufReader::new(std::fs::File::open(filename).unwrap());
-        for (line, subject) in f.lines().enumerate() {
-            let subject = subject.unwrap();
-            let length = subject.len();
-            let subject = CString::new(subject.into_bytes()).unwrap();
-            println!("{line} subject: {subject:?}");
+    let f = std::io::BufReader::new(std::fs::File::open(filename).unwrap());
+    for (line_no, subject) in f.lines().enumerate() {
+        let subject = subject.unwrap();
+        let length = subject.len();
+        let subject = CString::new(subject).unwrap();
+        eprintln!("{line_no} subject: {subject:?}");
 
-            let match_data = pcre2_match_data_create_from_pattern_8(re, NULL);
-            let rc = pcre2_match_8(re, subject.as_ptr().cast(), length, 0, 0, match_data, NULL);
-            if rc >= 0 {
-                debug_assert_ne!(rc, 0);
-                let rc: usize = rc.try_into().unwrap();
+        let match_data = unsafe { pcre2_match_data_create_from_pattern_8(re, NULL) };
+        let rc =
+            unsafe { pcre2_match_8(re, subject.as_ptr().cast(), length, 0, 0, match_data, NULL) };
+        if rc >= 0 {
+            debug_assert_ne!(rc, 0);
+            let rc = usize::try_from(rc).unwrap();
 
+            let ovector = unsafe {
                 let ovector = pcre2_get_ovector_pointer_8(match_data);
-                let ovector = std::slice::from_raw_parts(ovector, rc * 2);
-                debug_assert!(ovector.len() >= 4);
-                println!("Match succeeded at offset {:?}", ovector.get_unchecked(0));
+                std::slice::from_raw_parts(ovector, rc * 2)
+            };
+            debug_assert!(ovector.len() >= 4);
+            eprintln!("Match succeeded at offset {:?}", ovector[0]);
 
-                let substring_start = subject.as_ptr().add(*ovector.get_unchecked(2)).cast();
-                let substring_length = ovector.get_unchecked(3) - ovector.get_unchecked(2);
-                let slice = std::slice::from_raw_parts(substring_start, substring_length);
-                let result = String::from_utf8_unchecked(slice.to_owned());
-                println!("found: {result}");
-                send_result(result);
-            }
+            let substring = {
+                let begin = ovector[2];
+                let end = ovector[3];
+                &subject.as_bytes()[begin..end]
+            };
+            let result = String::from_utf8(substring.to_owned()).unwrap();
+            eprintln!("found: {result}");
+            send_result(result);
+        }
+        unsafe {
             pcre2_match_data_free_8(match_data);
         }
+    }
+    unsafe {
         pcre2_code_free_8(re);
-    };
+    }
 }
