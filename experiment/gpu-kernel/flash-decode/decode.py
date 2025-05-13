ccb811950866f1a33dd687b76fa5b7ec5941dead
Foreverhighness <Foreverhighness@github.com>
2025-05-13 17:37:38 +0800

    ✨ (flash-decode): Add reduce function.

--- experiment/gpu-kernel/flash-decode/decode.py
+++ experiment/gpu-kernel/flash-decode/decode.py
@@ -9,7 +9,7 @@ def decode_map(
     Q: tl.tensor, # [b, n, d]
     K: tl.tensor, # [b, n, s, d]
     V: tl.tensor, # [b, n, s, d]
-    Out: tl.tensor, # [b, n, d]
+    MidO: tl.tensor, # [b, n, n_split, d]
     L: tl.tensor,
     stride_qz: int, stride_qh: int, stride_qm: int, stride_qk: int,
     stride_kz: int, stride_kh: int, stride_kn: int, stride_kk: int,
@@ -28,7 +28,7 @@ def decode_map(
     offs_d = tl.arange(0, d)
 
     # Load Q ∈ d
-    qq = tl.load(Q + offs_d)
+    q = tl.load(Q + offs_d)
 
     # 初始化 Shared Memory 上的变量
     max = -float('inf')
@@ -36,6 +36,7 @@ def decode_map(
     O_i = tl.zeros([d], dtype=tl.float32)
 
     scale: tl.constexpr = d ** -0.5
+    q *= scale # scale ops: d
 
     # 主循环处理K/V块
     for n in range(0, seq_len, B_col):
@@ -54,8 +55,8 @@ def decode_map(
         v_j = tl.load(v_ptrs, mask=v_mask, other=0.0)
 
         # 计算 S_ij = Q_i @ K_j^T ∈ B_col
-        score_ij = tl.sum(qq[None, :] * k_j, axis=1)
-        score_ij = score_ij * scale
+        score_ij = tl.sum(q[None, :] * k_j, axis=1)
+        # score_ij = score_ij * scale # scale ops: seq_len / B_col * B_col
 
         # 在线softmax更新 max, S_ij ∈ B_col
         max_new = tl.maximum(max, tl.max(score_ij, axis=0))
@@ -76,44 +77,112 @@ def decode_map(
     O_i = O_i / l_sum
 
     # 写回输出
+    o_ptrs = MidO + offs_d
+    tl.store(o_ptrs, O_i)
+
+@triton.jit
+def decode_reduce(
+    MidO: tl.tensor, # [b, n, n_split, d]
+    Logsumexp: tl.tensor, # [b, n, n_split]
+    Out: tl.tensor, # [b, n, d]
+    stride_midOb: int, stride_midOn: int, stride_midOs: int, stride_midOd: int,
+    stride_Lb: int, stride_Ln: int, stride_Ld: int,
+    stride_Ob: int, stride_On: int, stride_Od: int,
+    seq_len: int,
+    d: tl.constexpr,
+    NUM_SPLITS: tl.constexpr,
+):
+    offs_d = tl.arange(0, d)
+    offs_mido = offs_d
+
+    split_size = tl.cdiv(seq_len, NUM_SPLITS)
+
+    max = -float('inf')
+    l_sum = 0.0
+    O = tl.zeros([d], dtype=tl.float32)
+
+    # Reduce Split
+    for id_split in range(0, NUM_SPLITS):
+        # 计算当前 split 的边界
+        split_start = id_split * split_size
+        split_end = tl.minimum(split_start + split_size, seq_len)
+
+        if split_start <= split_end:
+            midO_i = tl.load(MidO + offs_mido + id_split * stride_midOd)
+            logsumexp_i = tl.load(Logsumexp + id_split * stride_Ld)
+
+            # 更新 max
+            max_new = tl.maximum(logsumexp_i, max)
+
+            # 更新累加器
+            alpha = tl.exp(max - max_new)
+            O = O * alpha
+            exp_logic = tl.exp(logsumexp_i - max_new)
+            O = O + exp_logic * midO_i
+
+            # 更新运行统计量
+            l_sum = l_sum * alpha + exp_logic
+            max = max_new
+
+    # epilogue
+    # 最终归一化
+    O = O / l_sum
+
+    # 写回输出
     o_ptrs = Out + offs_d
-    tl.store(o_ptrs, O_i.to(Out.dtype.element_ty))
+    tl.store(o_ptrs, O.to(Out.dtype.element_ty))
 
-def flash_decode(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
-    assert q.dtype == k.dtype == v.dtype == torch.float16
-    assert q.shape[0] == k.shape[0] == v.shape[0]
-    assert q.shape[1] == k.shape[1] == v.shape[1]
-    assert k.shape[2] == v.shape[2]
-    assert q.shape[-1] == k.shape[-1] == v.shape[-1]
 
-    batch_size, num_heads, seq_len, dim = k.shape
+def flash_decode(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:
+    assert Q.dtype == K.dtype == V.dtype == torch.float16
+    assert Q.shape[0] == K.shape[0] == V.shape[0]
+    assert Q.shape[1] == K.shape[1] == V.shape[1]
+    assert K.shape[2] == V.shape[2]
+    assert Q.shape[-1] == K.shape[-1] == V.shape[-1]
+
+    batch_size, num_heads, seq_len, dim = K.shape
 
     B_col = 64
+    NUM_SPLITS = 1
 
-    out = torch.empty_like(q)
+    Out = torch.empty_like(Q)
     # print("out", out.shape)
-    L = torch.empty([B_col], device=q.device, dtype=torch.float32)
+    MidO = torch.empty((batch_size, num_heads, NUM_SPLITS, dim), device=Q.device, dtype=torch.float32)
+    Logsumexp = torch.empty((batch_size, num_heads, NUM_SPLITS), device=Q.device, dtype=torch.float32)
 
-    grid = (triton.cdiv(1, 128), batch_size * num_heads, 1)
-    stride_q = (q.stride(0), q.stride(1), 1, q.stride(-1))
-    stride_k = (k.stride(0), k.stride(1), k.stride(2), k.stride(3))
-    stride_v = (v.stride(0), v.stride(1), v.stride(2), v.stride(3))
-    stride_out = (out.stride(0), out.stride(1), 1, out.stride(-1))
-    print(stride_q, stride_k, stride_v, stride_out)
+    grid = (NUM_SPLITS, batch_size * num_heads, 1)
+    stride_q = (Q.stride(0), Q.stride(1), 1, Q.stride(-1))
+    stride_k = (K.stride(0), K.stride(1), K.stride(2), K.stride(3))
+    stride_v = (V.stride(0), V.stride(1), V.stride(2), V.stride(3))
+    stride_midO = (MidO.stride(0), MidO.stride(1), MidO.stride(2), MidO.stride(3))
+    stride_L = (Logsumexp.stride(0), Logsumexp.stride(1), Logsumexp.stride(2))
+    stride_O = (Out.stride(0), Out.stride(1), Out.stride(2))
+    print(stride_q, stride_k, stride_v, stride_midO)
     decode_map[grid](
-        q, k, v, out, L,
+        Q, K, V, MidO, Logsumexp,
         *stride_q,
         *stride_k,
         *stride_v,
-        *stride_out,
+        *stride_midO,
         seq_len=seq_len,
         B_col=B_col,
         d=dim,
         num_warps=4,
         num_stages=3,
+        NUM_SPLITS=NUM_SPLITS,
     )
-    # print("L:",  L)
-    return out
+    # print("MidO:", MidO)
+    decode_reduce[(batch_size, num_heads, 1)](
+        MidO, Logsumexp, Out,
+        *stride_midO,
+        *stride_L,
+        *stride_O,
+        seq_len=seq_len,
+        d=dim,
+        NUM_SPLITS=NUM_SPLITS,
+    )
+    # print("O:",  Out)
+    return Out
 
 # --------------------------
 # 测试代码
